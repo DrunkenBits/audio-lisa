55076336cefc7cf12441167bf6690131a4277ed1
Luca Barone <baro.luc@gmail.com>
2017-09-23 19:48:10 +0200

    Attempt ad making a sine wave, but it's time for dinner

--- index.js
+++ index.js
@@ -12,16 +12,19 @@ const args = require("yargs")
   .argv
 ;
 
+const data = [];
+const data2 = [];
+let format = null;
+
 const wavStream = fs.createReadStream(args.target);
 const reader = wav.Reader();
 
-reader.on("readable", (format) => {
+reader.on("readable", (f) => {
   //reader.pipe(new Speaker(format));
+  format = f;
 });
 
 wavStream.pipe(reader);
-
-const data = [];
 
 reader.on("data", (bytes) => {
   for (let i = 0; i < bytes.byteLength; i+=2) {
@@ -31,5 +34,12 @@ reader.on("data", (bytes) => {
 
 reader.on("end", () => {
   console.log(data.length);
+  
+  const sinLen = format.sampleRate * 2;
+  const sinFreq = 440 / (2 * Math.PI);
+  
+  for (var i = 0; i < sinLen; i++) {
+    data2.push((65536 * 0.5) * Math.sin()
+  }
 });
 
